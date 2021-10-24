383bf7c71a095544b31258d9cbe55f357189f699
recce <small_teams_behind_enemy_lines@protonmail.com>
2021-10-23 17:46:52 -0700

    increment version

--- awcy_nfo/__init__.py
+++ awcy_nfo/__init__.py
@@ -1,15 +1,12 @@
 # allow module import using: 'from awcy_nfo import ReadMe'
 from .readme import ReadMe  # noqa
 
-# TODO: doesnt work with pyinstaller
-# get version number from pyproject.toml
-# try:
-#    from importlib.metadata import version, PackageNotFoundError
-# except ImportError:
-#    from importlib_metadata import version, PackageNotFoundError
-# try:
-#    __version__ = version(__name__)
-# except PackageNotFoundError:
-#    __version__ = "(unknown)"
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
-__version__ = "0.1.0"
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError: # frozen app support doesnt work with importlib
+    __version__ = "0.1.1"
